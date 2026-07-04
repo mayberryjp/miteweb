2b6249d152e80d9448b68661c9bee99013e12fc9
JR Mayberry <rimayber@microsoft.com>
2026-07-04 17:42:06 +0900

    fix acknowledge all button

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,13 @@ import VueApexCharts from "vue3-apexcharts";
 import "vuetify/styles";
 import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import { mdi } from "vuetify/iconsets/mdi";
 
 const vuetify = createVuetify({
+  components,
+  directives,
   icons: {
     defaultSet: "mdi",
     sets: { mdi },
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vuetify from "vite-plugin-vuetify";
 import path from "path";
 import { readFileSync } from "fs";
 
 const { version } = JSON.parse(readFileSync(new URL("./package.json", import.meta.url), "utf-8"));
 
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [vue()],
   define: {
     __APP_VERSION__: JSON.stringify(version),
   },
